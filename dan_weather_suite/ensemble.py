b6e91873f378be0881d997649b8a6215b0f2b99b
Daniel Zurawski <djzurawski@gmail.com>
2023-11-25 14:37:32 -0700

    add NBM slr to dansemble

--- dan_weather_suite/ensemble.py
+++ dan_weather_suite/ensemble.py
@@ -13,7 +13,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy.typing import NDArray
 import os
+import pickle
 import requests
+from scipy.spatial import KDTree
 import time as ttime
 from typing import Tuple
 import xarray as xr
@@ -25,7 +27,7 @@ logging.basicConfig(format="%(asctime)s %(message)s", level=logging.INFO)
 logger = logging.getLogger("ensemble")
 
 
-def parse_np_datetime64(t: np.datetime64):
+def parse_np_datetime64(t: np.datetime64) -> datetime:
     return isoparse(str(t)).replace(tzinfo=timezone.utc)
 
 
@@ -37,7 +39,37 @@ def round_to_nearest(x: float, options: Iterable[float] = [0.25, 0.4, 0.5]) -> f
     return closest
 
 
-def set_ds_extent(ds, extent: regions.Extent):
+def haversine(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+    R = 6371  # Earth radius in kilometers
+
+    dLat = np.radians(lat2 - lat1)
+    dLon = np.radians(lon2 - lon1)
+    a = np.sin(dLat / 2) * np.sin(dLat / 2) + np.cos(np.radians(lat1)) * np.cos(
+        np.radians(lat2)
+    ) * np.sin(dLon / 2) * np.sin(dLon / 2)
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+    distance = R * c
+    return distance
+
+
+def swe_to_in(units: str) -> float:
+    IN_PER_M = 39.37
+    IN_PER_MM = 1 / 25.4
+    WATER_DENSITY = 1000  # kg/m3
+
+    if units == "m":
+        conversion = IN_PER_M
+    elif units == "mm":
+        conversion = IN_PER_MM
+    elif units == "kg m**-2":
+        conversion = (1 / WATER_DENSITY) * IN_PER_M
+    else:
+        raise ValueError(f"Unimplemented Unit conversion: {units} to in")
+
+    return conversion
+
+
+def set_ds_extent(ds: xr.Dataset, extent: regions.Extent) -> xr.Dataset:
     left = extent.left
     right = extent.right
     top = extent.top
@@ -75,7 +107,7 @@ def download_and_combine_gribs(urls: list[Tuple[str, dict]], threads=1) -> bytes
         return concatenated_bytes
 
 
-class EnsembleLoader(ABC):
+class ModelLoader(ABC):
     def __init__(self):
         self.step_size = 6
         self.forecast_length = 240
@@ -120,14 +152,14 @@ class EnsembleLoader(ABC):
                 try:
                     logger.info("Saving to NETCDF")
                     ds.to_netcdf(self.netcdf_file, mode="a")
-                    break
+                    logger.info("Up to date")
+                    return True
                 except Exception as e:
                     logger.error(f"Error saving NETCDF {self.netcdf_file}: {e}")
                     ttime.sleep(3)
                     retries += 1
 
-        logger.info("Up to date")
-        return True
+            return False
 
     @abstractmethod
     def download_grib(self):
@@ -156,11 +188,11 @@ class EnsembleLoader(ABC):
 
         return ds
 
-    def open_dataset(self):
+    def open_dataset(self) -> xr.Dataset:
         return xr.open_dataset(self.netcdf_file)
 
 
-class GepsLoader(EnsembleLoader):
+class GepsLoader(ModelLoader):
     def __init__(self):
         super().__init__()
         self.forecast_hours = list(
@@ -214,7 +246,7 @@ class GepsLoader(EnsembleLoader):
             root_url,
         )
 
-    def process_grib(self):
+    def process_grib(self) -> xr.Dataset:
         ds = super().process_grib()
         ds = ds.rename({"unknown": "tp"})
         ds["tp"].attrs["units"] = "mm"
@@ -226,7 +258,7 @@ class GepsLoader(EnsembleLoader):
         return ds
 
 
-class EpsLoader(EnsembleLoader):
+class EpsLoader(ModelLoader):
     def __init__(self):
         super().__init__()
         self.forecast_hours = list(
@@ -264,7 +296,7 @@ class EpsLoader(EnsembleLoader):
         latest_init = self.get_latest_init()
         if cycle is None:
             cycle = latest_init.hour
-        logger.info(f"EPS downloading {latest_init}")
+        logger.info(f"EPS downloading {cycle}")
         self.client.retrieve(
             time=cycle,
             stream="enfo",
@@ -274,7 +306,7 @@ class EpsLoader(EnsembleLoader):
         )
 
 
-class GefsLoader(EnsembleLoader):
+class GefsLoader(ModelLoader):
     def __init__(self):
         super().__init__()
         self.forecast_hours = list(
@@ -352,7 +384,7 @@ class GefsLoader(EnsembleLoader):
 
         return base_url, params
 
-    def process_grib(self):
+    def process_grib(self) -> xr.Dataset:
         ds = super().process_grib()
         ds["tp"] = ds.tp.cumsum(dim="step")
         ds["tp"].attrs["long_name"] = "Total Precipitation"
@@ -362,25 +394,123 @@ class GefsLoader(EnsembleLoader):
         return ds
 
 
-def swe_to_in(units: str):
-    IN_PER_M = 39.37
-    IN_PER_MM = 1 / 25.4
-    WATER_DENSITY = 1000  # kg/m3
+class NbmLoader(ModelLoader):
+    def __init__(self):
+        super().__init__()
+        self.forecast_hours = list(
+            range(6, self.forecast_length + self.step_size, self.step_size)
+        )
+        self.grib_file = "grib/nbm.grib"
+        self.netcdf_file = "grib/nbm.nc"
 
-    if units == "m":
-        conversion = IN_PER_M
-    elif units == "mm":
-        conversion = IN_PER_MM
-    elif units == "kg m**-2":
-        conversion = (1 / WATER_DENSITY) * IN_PER_M
-    else:
-        raise ValueError(f"Unimplemented Unit conversion: {units} to in")
+    def get_latest_init(self) -> datetime:
+        current_utc = datetime.utcnow()
+        current_utc_time = current_utc.time()
 
-    return conversion
+        release_00z = time(1, 15)
+        release_12z = time(13, 15)
+
+        if release_00z <= current_utc_time < release_12z:
+            return datetime(current_utc.year, current_utc.month, current_utc.day, 0, 0)
+        elif current_utc_time >= release_12z:
+            return datetime(current_utc.year, current_utc.month, current_utc.day, 12, 0)
+        else:
+            previous_day = current_utc - timedelta(days=1)
+            return datetime(
+                previous_day.year, previous_day.month, previous_day.day, 12, 0
+            )
+
+    def url_formatter(self, init_dt: datetime, fhour: int) -> Tuple[str, dict]:
+        day_str = init_dt.strftime("%Y%m%d")
+        cycle_str = str(init_dt.hour).zfill(2)
+        fhour_str = str(fhour).zfill(3)
+
+        extent = regions.PRISM_EXTENT
+        left = extent.left
+        right = extent.right
+        top = extent.top
+        bottom = extent.bottom
+
+        base_url = "https://nomads.ncep.noaa.gov/cgi-bin/filter_blend.pl"
+
+        params = {
+            "dir": f"/blend.{day_str}/{cycle_str}/core",
+            "file": f"blend.t{cycle_str}z.core.f{fhour_str}.co.grib2",
+            "var_SNOWLR": "on",
+            "subregion": "",
+            "toplat": top,
+            "leftlon": left,
+            "rightlon": right,
+            "bottomlat": bottom,
+        }
+
+        return base_url, params
+
+    def download_grib(self, cycle=None):
+        init_dt = self.get_latest_init()
+        if cycle is not None:
+            init_dt = init_dt.replace(hour=cycle)
+
+        # Need to download serially...slowly because of NOAA grib server request limits
+        grib_bytes = []
+        for fhour in self.forecast_hours:
+            url, params = self.url_formatter(
+                init_dt,
+                fhour,
+            )
+            grib_bytes.append(download_bytes(url, params))
+            ttime.sleep(0.2)
+
+        concatenated_bytes = b"".join(
+            result for result in grib_bytes if result is not None
+        )
+        with open(self.grib_file, "wb") as f:
+            f.write(concatenated_bytes)
+
+    def process_grib(self) -> xr.Dataset:
+        ds = xr.open_dataset(self.grib_file)
+        ds = ds.rename({"unknown": "slr"})
+        ds["longitude"] = (ds["longitude"] + 180) % 360 - 180
+        return ds
+
+    def create_kdtree(self) -> KDTree:
+        "Create KD tree of ds coordinates"
+        ds = self.open_dataset()
+        pairs = np.dstack((ds.longitude, ds.latitude)).reshape(-1, 2)
+        tree = KDTree(pairs)
+        with open("nbm-tree.pkl", "wb") as f:
+            pickle.dump(tree, f)
+
+        return tree
+
+    def forecast_slr(self, lon, lat) -> NDArray:
+        ds = self.open_dataset()
+        k_nearest = 36
+        if not os.path.exists("nbm-tree.pkl"):
+            tree = self.create_kdtree()
+        else:
+            with open("nbm-tree.pkl", "rb") as f:
+                tree = pickle.load(f)
+
+        pairs = np.dstack((ds.longitude, ds.latitude)).reshape(-1, 2)
+        distances, indicies = tree.query([lon, lat], k_nearest)
+        nearest_points = pairs[indicies]
+
+        # (haversine dist, coord array indices)
+        nearest_points_idx = [
+            (haversine(lat, lon, nbm_lat, nbm_lon), idx)
+            for (nbm_lon, nbm_lat), idx in zip(nearest_points, indicies)
+        ]
+
+        nearest_point_km, nearest_idx = min(nearest_points_idx, key=lambda x: x[0])
+        logger.info(f"Nearest NBM: {pairs[nearest_idx]} {round(nearest_point_km,2)}km")
+
+        nearest_row, nearest_col = divmod(nearest_idx, ds.latitude.shape[1])
+        return ds.slr[..., nearest_row, nearest_col]
 
 
 class Ensemble:
-    def __init__(self, loader: EnsembleLoader, name="Ens", plume_color="red"):
+    def __init__(self, loader: ModelLoader, name="Ens", plume_color="red"):
         self.loader = loader
         self.name = name
         self.plume_color = plume_color
@@ -400,7 +530,7 @@ class Ensemble:
         return self.ds.valid_time.values[0] == self.ds.time.values
 
     def point_plumes(
-        self, lon: float, lat: float, downscale=True
+        self, lon: float, lat: float, downscale=True, accum_snow=False
     ) -> Tuple[NDArray[np.datetime64], NDArray[float]]:
         ds = self.ds
         prepend_t0 = not self._forecast_starts_at_init()
@@ -427,6 +557,15 @@ class Ensemble:
                 values = np.concatenate([[0], values])
 
             plumes.append(values)
+
+        plumes = np.array(plumes)
+
+        if accum_snow:
+            nbm = NbmLoader()
+            slr = nbm.forecast_slr(lon, lat).values
+            snow_rate = np.diff(plumes, axis=1) * slr
+            cumsnow = np.cumsum(snow_rate, axis=1)
+            plumes = cumsnow
 
         return times, np.array(plumes)
 
@@ -475,20 +614,22 @@ class Ensemble:
             )
 
 
-def download_loader_forecast(
-    loader_class: type[EnsembleLoader], cycle=None, force=False
-):
+def download_loader_forecast(loader_class: type[ModelLoader], cycle=None, force=False):
     return loader_class().download_forecast(cycle, force)
 
 
 def download_all_forecasts(cycle=None, force=False):
     with ProcessPoolExecutor() as executor:
-        gefs = executor.submit(download_loader_forecast, GefsLoader, cycle, force)
+        gefs = executor.submit(download_loader_forecast, GefsLoader, cycle, force=force)
         geps = executor.submit(download_loader_forecast, GepsLoader, cycle, force=force)
         eps = executor.submit(download_loader_forecast, EpsLoader, cycle, force=force)
 
         futures = [gefs, geps, eps]
-        return [f.result() for f in futures]
+        ensemble_res = [f.result() for f in futures]
+
+        nbm = NbmLoader()
+        nbm_res = nbm.download_forecast(cycle=cycle, force=force)
+        return ensemble_res + [nbm_res]
 
 
 def plot_compare(ens: Ensemble):
@@ -575,6 +716,124 @@ def plume_plot(lon, lat, title="", return_bytes: bool = False):
 
     axs[0].grid(axis="both", linestyle="--")
     axs[1].grid(axis="both", linestyle="--")
+
+    if return_bytes:
+        with io.BytesIO() as bio:
+            plt.savefig(bio, format="jpg", bbox_inches="tight")
+            return bio.getvalue()
+
+    plt.show()
+
+
+def plume_plot_snow(lon, lat, title="", return_bytes: bool = False):
+    eps = Ensemble(EpsLoader(), "ECMWF ENS", "green")
+    gefs = Ensemble(GefsLoader(), "GEFS", "red")
+    geps = Ensemble(GepsLoader(), "CMCE", "blue")
+
+    ensembles = [geps, gefs, eps]
+    nbm = NbmLoader()
+    slr = nbm.forecast_slr(lon, lat)
+
+    fig, axs = plt.subplots(2, 2, figsize=(16, 10), sharey="row")
+    fig.suptitle(f"{title} lat: {lat} lon: {lon}")
+    plt.tight_layout(
+        pad=3,
+    )
+
+    all_precip = []
+    all_snow = []
+    for ensemble in ensembles:
+        times, plumes = ensemble.point_plumes(lon, lat)
+
+        snow_plumes = np.zeros(plumes.shape)
+        precip_rate = np.diff(plumes, axis=1)
+        snow_rate = precip_rate * slr.values
+        snow_plumes[:, 1:] = np.cumsum(snow_rate, axis=1)
+
+        for plume, snow_plume in zip(plumes, snow_plumes):
+            axs[0, 0].plot(
+                times, plume, color=ensemble.plume_color, alpha=0.3, linewidth=1
+            )
+
+            axs[1, 0].plot(
+                times, snow_plume, color=ensemble.plume_color, alpha=0.3, linewidth=1
+            )
+            all_precip.append(plume)
+            all_snow.append(snow_plume)
+
+        precip_mean = np.mean(plumes, axis=0)
+        snow_mean = np.mean(snow_plumes, axis=0)
+        axs[0, 0].plot(
+            times,
+            precip_mean,
+            color=ensemble.plume_color,
+            linewidth=3,
+            zorder=200,
+            label=ensemble.name,
+        )
+        axs[1, 0].plot(
+            times,
+            snow_mean,
+            color=ensemble.plume_color,
+            linewidth=3,
+            zorder=200,
+            label=ensemble.name,
+        )
+
+    axs[0, 0].legend()
+    axs[1, 0].legend()
+
+    # Boxplots
+    precip_boxplot_data = np.array(all_precip)
+    snow_boxplot_data = np.array(all_snow)
+    axs[0, 1].boxplot(precip_boxplot_data, showfliers=False, whis=(10, 90))
+    axs[1, 1].boxplot(snow_boxplot_data, showfliers=False, whis=(10, 90))
+
+    # SLR Line
+    ax_slr = axs[1, 1].twinx()
+    slr_x = np.arange(2, len(slr) + 2)
+    ax_slr.plot(slr_x, slr, color="gray", label="Snow Liquid Ratio")
+    ax_slr.set_ylim((0, 30))
+    ax_slr.legend()
+    ax_slr.set_ylabel("Snow:Liquid Ratio")
+
+    # Set xaxis labels
+    times_ticks = times[::2]
+    times_labels = [xtick_formatter(parse_np_datetime64(t)) for t in times_ticks]
+    axs[0, 0].set_xticks(times_ticks, labels=times_labels)
+    axs[1, 0].set_xticks(times_ticks, labels=times_labels)
+
+    axs[0, 1].set_xticks(np.arange(1, len(times) + 1, 2), labels=times_labels)
+    axs[1, 1].set_xticks(np.arange(1, len(times) + 1, 2), labels=times_labels)
+
+    # vertical line at day 7
+    day_7 = int(168 / 6)
+    axs[0, 0].axvline(times[day_7], color="gray", linestyle="--")
+    axs[1, 0].axvline(times[day_7], color="gray", linestyle="--")
+    axs[0, 1].axvline(day_7, color="gray", linestyle="--")
+    axs[1, 1].axvline(day_7, color="gray", linestyle="--")
+
+    # turn second y axis labels on
+    axs[0, 1].yaxis.set_tick_params(labelleft=True)
+    axs[1, 1].yaxis.set_tick_params(labelleft=True)
+
+    # Grid dotted lines
+    axs[0, 0].grid(axis="both", linestyle="--")
+    axs[1, 0].grid(axis="both", linestyle="--")
+    axs[0, 1].grid(axis="both", linestyle="--")
+    axs[1, 1].grid(axis="both", linestyle="--")
+
+    # Subplot titles
+    axs[0, 0].title.set_text("Precipitation")
+    axs[0, 1].title.set_text("Precipitation")
+    axs[1, 0].title.set_text("Snow")
+    axs[1, 1].title.set_text("Snow")
+
+    # Subplot ylabels
+    axs[0, 0].set_ylabel("Precip (in)")
+    axs[0, 1].set_ylabel("Precip (in)")
+    axs[1, 0].set_ylabel("Snow (in)")
+    axs[1, 1].set_ylabel("Snow (in)")
 
     if return_bytes:
         with io.BytesIO() as bio:
