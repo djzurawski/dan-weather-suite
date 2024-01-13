dd133c29df433df3de91a964089d2d937c23a400
Daniel Zurawski <djzurawski@gmail.com>
2024-01-12 20:09:48 -0700

    Hres dansemble hourly plots

--- dan_weather_suite/hres_ensemble.py
+++ dan_weather_suite/hres_ensemble.py
@@ -6,22 +6,10 @@ from dan_weather_suite.models.hiresw_arw import (
 from dan_weather_suite.models.hrrr import HrrrLoader
 from dan_weather_suite.models.nbm import NbmLoader
 from dan_weather_suite.models.rrfs import RrfsLoader
-from datetime import datetime
 import dan_weather_suite.utils as utils
 import io
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-def xtick_formatter(dt: datetime):
-    if dt.hour == 6:
-        return "06z"
-    if dt.hour == 12:
-        return "12z"
-    if dt.hour == 0:
-        return "00z\n" + dt.strftime("%b-%d")
-    else:
-        return ""
 
 
 def plume_plot(lon: float, lat: float, title="", return_bytes=False):
@@ -37,7 +25,7 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
     nbm = NbmLoader(short_term=True)
     slr_ds = nbm.forecast_slr(lon, lat)
 
-    fig, axs = plt.subplots(2, 2, figsize=(16, 10), sharey="row")
+    fig, axs = plt.subplots(2, 2, figsize=(16, 10))
     fig.suptitle(f"{title} lat: {lat} lon: {lon}")
     plt.tight_layout(
         pad=3,
@@ -64,7 +52,11 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
         snow[1:] = np.cumsum(snow_rate)
 
         axs[0, 0].plot(times, precip_in, label=model_name)
-        axs[1, 0].plot(times, snow, label=model_name)
+        axs[0, 1].plot(times, snow, label=model_name)
+
+        # hourly
+        axs[1, 0].plot(times, np.gradient(precip_in), label=model_name)
+        axs[1, 1].plot(times, np.gradient(snow), label=model_name)
 
         all_precip.append(precip_in)
         all_snow.append(snow)
@@ -82,7 +74,8 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
 
             precip_in = conversion * precip_raw.values
 
-            slr = slr_ds.interp(step=member_forecast.step).values[:-1]
+            slr_da = slr_ds.interp(step=member_forecast.step)
+            slr = slr_da.values[:-1]
 
             snow = np.zeros(precip_in.shape)
             precip_rate = np.diff(precip_in)
@@ -93,12 +86,37 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
                 axs[0, 0].plot(
                     times, precip_in, label=model_name, color="deepskyblue", alpha=0.75
                 )
+                # hourly
                 axs[1, 0].plot(
+                    times,
+                    np.gradient(precip_in),
+                    label=model_name,
+                    color="deepskyblue",
+                    alpha=0.75,
+                )
+
+                axs[0, 1].plot(
                     times, snow, label=model_name, color="deepskyblue", alpha=0.75
+                )
+                # hourly
+                axs[1, 1].plot(
+                    times,
+                    np.gradient(snow),
+                    label=model_name,
+                    color="deepskyblue",
+                    alpha=0.75,
                 )
             else:
                 axs[0, 0].plot(times, precip_in, color="deepskyblue", alpha=0.75)
-                axs[1, 0].plot(times, snow, color="deepskyblue", alpha=0.75)
+                axs[0, 1].plot(times, snow, color="deepskyblue", alpha=0.75)
+
+                # hourly
+                axs[1, 0].plot(
+                    times, np.gradient(precip_in), color="deepskyblue", alpha=0.75
+                )
+                axs[1, 1].plot(
+                    times, np.gradient(snow), color="deepskyblue", alpha=0.75
+                )
 
             all_precip.append(precip_in)
             all_snow.append(snow)
@@ -109,48 +127,63 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
     axs[0, 0].plot(
         times, precip_mean, linewidth=3, color="black", zorder=200, label="mean"
     )
-    axs[1, 0].plot(
+    axs[0, 1].plot(
         times, snow_mean, linewidth=3, color="black", zorder=200, label="mean"
+    )
+
+    # hourly
+    axs[1, 0].plot(
+        times,
+        np.gradient(precip_mean),
+        linewidth=3,
+        color="black",
+        zorder=200,
+        label="mean",
+    )
+    axs[1, 1].plot(
+        times,
+        np.gradient(snow_mean),
+        linewidth=3,
+        color="black",
+        zorder=200,
+        label="mean",
     )
 
     axs[0, 0].legend()
     axs[1, 0].legend()
 
-    # Boxplots
-    precip_boxplot_data = np.array(all_precip)
-    snow_boxplot_data = np.array(all_snow)
-    axs[0, 1].boxplot(precip_boxplot_data, showfliers=False, whis=(10, 90))
-    axs[1, 1].boxplot(snow_boxplot_data, showfliers=False, whis=(10, 90))
-
     # SLR Line
-    ax_slr = axs[1, 1].twinx()
-    slr_x = np.arange(1, len(slr) + 1)
-    ax_slr.plot(slr_x, slr, color="gray", label="Snow Liquid Ratio")
+    ax_slr = axs[0, 1].twinx()
+    ax_slr.plot(
+        slr_da.valid_time.values, slr_da, color="gray", label="Snow Liquid Ratio"
+    )
     ax_slr.set_ylim((0, 30))
     ax_slr.legend()
     ax_slr.set_ylabel("Snow:Liquid Ratio")
 
-    # turn second y axis labels on
-    axs[0, 1].yaxis.set_tick_params(labelleft=True)
-    axs[1, 1].yaxis.set_tick_params(labelleft=True)
-
-    # Set xaxis labels for boxplots
-    tick_interval = 2
-    ticks = np.arange(0, len(times) + 1, tick_interval)
-    axs[0, 1].set_xticks(ticks, labels=ticks)
-    axs[1, 1].set_xticks(ticks, labels=ticks)
+    ax_slr = axs[1, 1].twinx()
+    ax_slr.plot(
+        slr_da.valid_time.values, slr_da, color="gray", label="Snow Liquid Ratio"
+    )
+    ax_slr.set_ylim((0, 30))
+    ax_slr.legend()
+    ax_slr.set_ylabel("Snow:Liquid Ratio")
 
     # Subplot titles
     axs[0, 0].title.set_text("Accumulated Precipitation")
-    axs[0, 1].title.set_text("Accumulated Precipitation")
-    axs[1, 0].title.set_text("Accumulated Snow")
-    axs[1, 1].title.set_text("Accumulated Snow")
+    axs[0, 1].title.set_text("Accumulated Snow")
+    axs[1, 0].title.set_text("Hourly Snow")
+    axs[1, 1].title.set_text("Hourly Snow")
 
     # Subplot ylabels
     axs[0, 0].set_ylabel("Precip (in)")
-    axs[0, 1].set_ylabel("Precip (in)")
-    axs[1, 0].set_ylabel("Snow (in)")
+    axs[0, 1].set_ylabel("Snow (in)")
+    axs[1, 0].set_ylabel("Precip (in)")
     axs[1, 1].set_ylabel("Snow (in)")
+
+    # Subplot xlabels
+    axs[1, 0].set_xlabel("Time (UTC)")
+    axs[1, 1].set_xlabel("Time (UTC)")
 
     if return_bytes:
         with io.BytesIO() as bio:
