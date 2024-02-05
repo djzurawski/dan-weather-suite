76dacf14f3dba1a0c2238f7756634f257a43d6ba
Daniel Zurawski <djzurawski@gmail.com>
2024-02-04 18:39:55 -0700

    Give plot axs names Better way of getting plot legend fo unique names hres plume_plot handles failing rrfs hres clean grib on download by default

--- dan_weather_suite/hres_ensemble.py
+++ dan_weather_suite/hres_ensemble.py
@@ -8,10 +8,13 @@ from dan_weather_suite.models.nbm import NbmLoader
 from dan_weather_suite.models.rrfs import RrfsLoader
 import dan_weather_suite.utils as utils
 import io
+import logging
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+# TODO handle not updated models
+# TODO reduce nesting
 def plume_plot(lon: float, lat: float, title="", return_bytes=False):
     LOADERS = {
         "ARW": HireswArwLoader(),
@@ -31,6 +34,11 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
         pad=3,
     )
 
+    acc_precip_ax = axs[0, 0]
+    acc_snow_ax = axs[0, 1]
+    hr_precip_ax = axs[1, 0]
+    hr_snow_ax = axs[1, 1]
+
     all_precip = []
     all_snow = []
     for model_name, loader in LOADERS.items():
@@ -44,50 +52,55 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
 
         precip_in = conversion * precip_raw.values
 
-        slr = slr_ds.interp(step=point_forecast.step).values[:-1]
+        slr_da = slr_ds.interp(step=point_forecast.step)
+        slr = slr_da.values[:-1]
 
         snow = np.zeros(precip_in.shape)
         precip_rate = np.diff(precip_in)
         snow_rate = precip_rate * slr
         snow[1:] = np.cumsum(snow_rate)
 
-        axs[0, 0].plot(times, precip_in, label=model_name)
-        axs[0, 1].plot(times, snow, label=model_name)
+        acc_precip_ax.plot(times, precip_in, label=model_name)
+        acc_snow_ax.plot(times, snow, label=model_name)
 
         # hourly
-        axs[1, 0].plot(times, np.gradient(precip_in), label=model_name)
-        axs[1, 1].plot(times, np.gradient(snow), label=model_name)
+        hr_precip_ax.plot(times, np.gradient(precip_in), label=model_name)
+        hr_snow_ax.plot(times, np.gradient(snow), label=model_name)
 
         all_precip.append(precip_in)
         all_snow.append(snow)
 
     for model_name, loader in ENSEMBLE_LOADERS.items():
-        ds = loader.open_dataset()
-        kdtree = loader.get_kdtree()
-        point_forecast = utils.nearest_neighbor_forecast(ds, kdtree, lon, lat)
+        try:
+            ds = loader.open_dataset()
+            kdtree = loader.get_kdtree()
+            point_forecast = utils.nearest_neighbor_forecast(ds, kdtree, lon, lat)
 
-        for member in point_forecast.number.values:
-            member_forecast = point_forecast.sel(number=member)
-            precip_raw = member_forecast.tp
-            conversion = utils.swe_to_in(precip_raw.units)
-            times = precip_raw.valid_time.values
+            for member in point_forecast.number.values:
+                member_forecast = point_forecast.sel(number=member)
+                precip_raw = member_forecast.tp
+                conversion = utils.swe_to_in(precip_raw.units)
+                times = precip_raw.valid_time.values
 
-            precip_in = conversion * precip_raw.values
+                precip_in = conversion * precip_raw.values
 
-            slr_da = slr_ds.interp(step=member_forecast.step)
-            slr = slr_da.values[:-1]
+                slr_da = slr_ds.interp(step=member_forecast.step)
+                slr = slr_da.values[:-1]
 
-            snow = np.zeros(precip_in.shape)
-            precip_rate = np.diff(precip_in)
-            snow_rate = precip_rate * slr
-            snow[1:] = np.cumsum(snow_rate)
+                snow = np.zeros(precip_in.shape)
+                precip_rate = np.diff(precip_in)
+                snow_rate = precip_rate * slr
+                snow[1:] = np.cumsum(snow_rate)
 
-            if member == 0:
-                axs[0, 0].plot(
-                    times, precip_in, label=model_name, color="deepskyblue", alpha=0.75
+                acc_precip_ax.plot(
+                    times,
+                    precip_in,
+                    label=model_name,
+                    color="deepskyblue",
+                    alpha=0.75,
                 )
                 # hourly
-                axs[1, 0].plot(
+                hr_precip_ax.plot(
                     times,
                     np.gradient(precip_in),
                     label=model_name,
@@ -95,44 +108,36 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
                     alpha=0.75,
                 )
 
-                axs[0, 1].plot(
+                acc_snow_ax.plot(
                     times, snow, label=model_name, color="deepskyblue", alpha=0.75
                 )
                 # hourly
-                axs[1, 1].plot(
+                hr_snow_ax.plot(
                     times,
                     np.gradient(snow),
                     label=model_name,
                     color="deepskyblue",
                     alpha=0.75,
                 )
-            else:
-                axs[0, 0].plot(times, precip_in, color="deepskyblue", alpha=0.75)
-                axs[0, 1].plot(times, snow, color="deepskyblue", alpha=0.75)
 
-                # hourly
-                axs[1, 0].plot(
-                    times, np.gradient(precip_in), color="deepskyblue", alpha=0.75
-                )
-                axs[1, 1].plot(
-                    times, np.gradient(snow), color="deepskyblue", alpha=0.75
-                )
+                all_precip.append(precip_in)
+                all_snow.append(snow)
 
-            all_precip.append(precip_in)
-            all_snow.append(snow)
+        except Exception as e:
+            logging.error(f"Could lot plot {model_name}: {e}")
 
     precip_mean = np.mean(all_precip, axis=0)
     snow_mean = np.mean(all_snow, axis=0)
 
-    axs[0, 0].plot(
+    acc_precip_ax.plot(
         times, precip_mean, linewidth=3, color="black", zorder=200, label="mean"
     )
-    axs[0, 1].plot(
+    acc_snow_ax.plot(
         times, snow_mean, linewidth=3, color="black", zorder=200, label="mean"
     )
 
     # hourly
-    axs[1, 0].plot(
+    hr_precip_ax.plot(
         times,
         np.gradient(precip_mean),
         linewidth=3,
@@ -140,7 +145,7 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
         zorder=200,
         label="mean",
     )
-    axs[1, 1].plot(
+    hr_snow_ax.plot(
         times,
         np.gradient(snow_mean),
         linewidth=3,
@@ -149,11 +154,17 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
         label="mean",
     )
 
-    axs[0, 0].legend()
-    axs[1, 0].legend()
+    # remove duplicate labels
+    # https://stackoverflow.com/a/56253636
+    handles, labels = acc_precip_ax.get_legend_handles_labels()
+    unique = [
+        (h, l) for i, (h, l) in enumerate(zip(handles, labels)) if l not in labels[:i]
+    ]
+    acc_precip_ax.legend(*zip(*unique))
+    hr_precip_ax.legend(*zip(*unique))
 
     # SLR Line
-    ax_slr = axs[0, 1].twinx()
+    ax_slr = acc_snow_ax.twinx()
     ax_slr.plot(
         slr_da.valid_time.values, slr_da, color="gray", label="Snow Liquid Ratio"
     )
@@ -161,7 +172,7 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
     ax_slr.legend()
     ax_slr.set_ylabel("Snow:Liquid Ratio")
 
-    ax_slr = axs[1, 1].twinx()
+    ax_slr = hr_snow_ax.twinx()
     ax_slr.plot(
         slr_da.valid_time.values, slr_da, color="gray", label="Snow Liquid Ratio"
     )
@@ -170,26 +181,26 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
     ax_slr.set_ylabel("Snow:Liquid Ratio")
 
     # Grid dotted lines
-    axs[0, 0].grid(axis="both", linestyle="--")
-    axs[1, 0].grid(axis="both", linestyle="--")
-    axs[0, 1].grid(axis="both", linestyle="--")
-    axs[1, 1].grid(axis="both", linestyle="--")
+    acc_precip_ax.grid(axis="both", linestyle="--")
+    hr_precip_ax.grid(axis="both", linestyle="--")
+    acc_snow_ax.grid(axis="both", linestyle="--")
+    hr_snow_ax.grid(axis="both", linestyle="--")
 
     # Subplot titles
-    axs[0, 0].title.set_text("Accumulated Precipitation")
-    axs[0, 1].title.set_text("Accumulated Snow")
-    axs[1, 0].title.set_text("Hourly Precip")
-    axs[1, 1].title.set_text("Hourly Snow")
+    acc_precip_ax.title.set_text("Accumulated Precipitation")
+    acc_snow_ax.title.set_text("Accumulated Snow")
+    hr_precip_ax.title.set_text("Hourly Precip")
+    hr_snow_ax.title.set_text("Hourly Snow")
 
     # Subplot ylabels
-    axs[0, 0].set_ylabel("Precip (in)")
-    axs[0, 1].set_ylabel("Snow (in)")
-    axs[1, 0].set_ylabel("Precip (in)")
-    axs[1, 1].set_ylabel("Snow (in)")
+    acc_precip_ax.set_ylabel("Precip (in)")
+    acc_snow_ax.set_ylabel("Snow (in)")
+    hr_precip_ax.set_ylabel("Precip (in)")
+    hr_snow_ax.set_ylabel("Snow (in)")
 
     # Subplot xlabels
-    axs[1, 0].set_xlabel("Time (UTC)")
-    axs[1, 1].set_xlabel("Time (UTC)")
+    hr_precip_ax.set_xlabel("Time (UTC)")
+    hr_snow_ax.set_xlabel("Time (UTC)")
 
     if return_bytes:
         with io.BytesIO() as bio:
@@ -200,7 +211,7 @@ def plume_plot(lon: float, lat: float, title="", return_bytes=False):
     plt.show()
 
 
-def download_all_forecasts(cycle=None, force=False):
+def download_all_forecasts(cycle=None, force=True):
     LOADERS = {
         "ARW": HireswArwLoader(),
         "ARW2": HireswArw2Loader(),
@@ -211,4 +222,7 @@ def download_all_forecasts(cycle=None, force=False):
     }
 
     for name, loader in LOADERS.items():
-        loader.download_forecast(cycle=cycle, force=force)
+        try:
+            loader.download_forecast(cycle=cycle, force=force)
+        except Exception as e:
+            logging.error(f"Couldnt download {name} forecast:{e}")
